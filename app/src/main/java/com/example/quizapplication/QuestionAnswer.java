1f89162ee4830e434badaf41aa2fbfdea4603b19
SamiAtha <105737131+SamiAtha@users.noreply.github.com>
2022-05-21 11:27:36 +0700

    Our Quiz

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What year was CCIT founded?",
+            "What is the last version of Java?",
+            "Who is the founder of Apple?",
+            "Who is Founder Of Youtube?",
+            "The year that apple founder?"
+    };
+
+    public static String choices[][] = {
+            {"1945","2002","1900","2000","2022"},
+            {"Java SE18","Java SE10","J2SE 1.3","Java SE 2,3"},
+            {"Steve Jobs","Javed Karim","BJ Habibie,","Mark Zuckerberg","Jeff Broz"},
+            {"Javed Karim","Bill Gates","Jerry Yang","Satoshi Nakamoto","John Cena"},
+            {"1976","1980","1995","1998","2001"}
+    };
+
+    public static String correctAnswer[] = {
+            "2002",
+            "Java SE18",
+            "Steve Jobs",
+            "Javed Karim",
+            "1976"
+    };
+
+}
+
